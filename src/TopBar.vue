a7be573af06dcfdeb779d1d858837626d3320c65
guoliang.chen@21chinamall.com <guoliang.chen@21chinamall.com>
2016-12-22 11:38:42 +0800

    创建父母圈H5版

---
+++ src/TopBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <flexbox class="bg-color">
+      <flexbox-item>
+        <div class="flex-demo">返回</div>
+      </flexbox-item>
+      <flexbox-item>
+        <div class="flex-demo">父母圈</div>
+      </flexbox-item>
+      <flexbox-item>
+        <div class="flex-demo"></div>
+      </flexbox-item>
+    </flexbox>
+  </div>
+</template>
+<style scoped>
+    body{
+        background-color:#ff0000;
+    }
+
+    .bg-color {
+      background-color: #FFFFFF;
+    }
+
+   .flex-demo {
+      text-align: center;  /*文本居中显示*/
+      color: #000;
+      background-color: #FFFFFF;
+    }
+</style>
+<script>
+import {Divider,FlexboxItem,Flexbox} from 'vux/src/components'
+
+    export default{
+        data(){
+            return{
+            }
+        },
+        components:{
+          Divider,
+          FlexboxItem,
+          Flexbox
+        }
+    }
+
+</script>
---
+++ src/components/Hall.vue
@@ -0,0 +1,41 @@
+<template>
+  <top-bar></top-bar>
+  <group title="cell demo">
+    <cell title="Vux" value="Cool" is-link></cell>
+  </group>
+</template>
+
+<script>
+import Group from 'vux/src/components/group'
+import Cell from 'vux/src/components/cell'
+import TopBar from '../TopBar'
+import {Divider,FlexboxItem,Flexbox} from 'vux/src/components'
+
+export default {
+  components: {
+    Group,
+    Cell,
+    TopBar
+  },
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
